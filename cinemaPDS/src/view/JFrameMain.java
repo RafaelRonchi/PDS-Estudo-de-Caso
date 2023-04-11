107dad83a8c9fbf80f50f1fceb9dc3ac4f3ba386
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-04-11 08:27:49 -0300

    Jframe main login

--- cinemaPDS/src/view/JFrameMain.java
+++ cinemaPDS/src/view/JFrameMain.java
@@ -8,10 +8,14 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
 
 public class JFrameMain extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -34,14 +38,16 @@ public class JFrameMain extends JFrame {
 	 * Create the frame.
 	 */
 	public JFrameMain() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 859, 516);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 
 		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(374, 215, 89, 23);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose(); // fecha tela atual
@@ -51,7 +57,22 @@ public class JFrameMain extends JFrame {
 
 			}
 		});
+		contentPane.setLayout(null);
 		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Abrir sistema:");
+		lblNewLabel.setFont(new Font("Yu Gothic UI Light", Font.BOLD, 29));
+		lblNewLabel.setBounds(330, 79, 244, 66);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(305, 171, 260, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Usuário:");
+		lblNewLabel_1.setFont(new Font("Yu Gothic Light", Font.BOLD, 14));
+		lblNewLabel_1.setBounds(223, 172, 71, 25);
+		contentPane.add(lblNewLabel_1);
 	}
-
 }
