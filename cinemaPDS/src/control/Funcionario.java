179fe7c969418657c3188b6a7ea351a03b244227
nicolas <nicolas.rm2005@aluno.ifsc.edu.br>
2023-04-13 08:35:33 -0300

    mvc

---
+++ cinemaPDS/src/control/Funcionario.java
@@ -0,0 +1,40 @@
+package control;
+
+import java.time.LocalDate;
+
+public class Funcionario {
+
+	
+	
+	
+	
+	
+	private Long cpf;
+	private String nome;
+	private LocalDate dataNasc;
+	public Long getCpf() {
+		return cpf;
+	}
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public LocalDate getDataNasc() {
+		return dataNasc;
+	}
+	public void setDataNasc(LocalDate dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
